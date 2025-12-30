8c27fe37711d4484afa822319ce66e45ed179ffc
cgst13 <bsit.christiantolentino@gmail.com>
2025-12-30 21:47:43 +0800

    Fix: Switch to HashRouter to prevent 404s on refresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Playlist from './pages/Playlist';
 
 function App() {
-  // Determine basename: if running on GitHub Pages subdirectory, use repo name; otherwise root.
-  // This supports both 'cgst13.github.io/karaokecity' and 'custom-domain.com'
-  const basename = window.location.pathname.startsWith('/karaokecity') 
-    ? '/karaokecity' 
-    : '/';
-
   return (
-    <Router basename={basename}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/playlist/:id" element={<Playlist />} />
