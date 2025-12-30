e3afbf8456f5162914e7bfc076ec09a3e113e6dc
cgst13 <bsit.christiantolentino@gmail.com>
2025-12-30 21:36:02 +0800

    Fix: Add basename to Router for GitHub Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Playlist from './pages/Playlist';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/playlist/:id" element={<Playlist />} />
